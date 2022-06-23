63ef4ef066bce9c4ee9a650f5a851729ef91d0b0
ade-m <66313509+ade-m@users.noreply.github.com>
2022-06-23 11:25:48 +0700

    init tambahan

--- src/model/Enemy.java
+++ src/model/Enemy.java
@@ -5,7 +5,7 @@ public class Enemy {
     int hp;
     int attackPoin;
     public void attack(){
-
+            //tambahan
         //ini keterangan
         //tes samuel
         System.out.println("Serang!!!");
